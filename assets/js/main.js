c11fa9fb192b4b6b3c4f4036f9d0aaf8fb161cfa
BryamVC <bryamvega7@gmail.com>
2023-03-18 15:42:36 -0500

    add testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,3 +79,24 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+
+let swiper = new Swiper(".testimonials__container", {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+        },
+      },
+  });
