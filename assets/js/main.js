599732c1db06cf7b51606c5b6d4fea227d22e7bc
BryamVC <bryamvega7@gmail.com>
2023-03-19 07:20:23 -0500

    add contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,7 +85,7 @@ let swiper = new Swiper(".testimonials__container", {
     spaceBetween: 24,
     loop: true,
     grabCursor: true,
-    
+
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -100,3 +100,22 @@ let swiper = new Swiper(".testimonials__container", {
         },
       },
   });
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
